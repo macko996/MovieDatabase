6e884dff3683b2accee2a20f4cbe8ebfca4e85d4
Marjan Kolevski <Marjan.Kolevski@netcetera.com>
2020-12-21 11:57:15 +0100

    Added Fragments and RecyclerView

---
+++ app/src/main/java/com/example/moviedatabase/MoviesListFragment.kt
@@ -0,0 +1,45 @@
+package com.example.moviedatabase
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [MoviesListFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class MoviesListFragment : Fragment() {
+    private lateinit var movieRecyclerView: RecyclerView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val view = inflater.inflate(R.layout.fragment_movies_list, container, false)
+        movieRecyclerView = view.findViewById(R.id.movies_recycler_view)
+        movieRecyclerView.layoutManager = GridLayoutManager(context,2)
+
+        return view
+    }
+
+    companion object {
+        /**
+         * Use this factory method to create a new instance of
+         * @return A new instance of fragment MoviesListFragment.
+         */
+        fun newInstance() = MoviesListFragment()
+    }
+
+}
