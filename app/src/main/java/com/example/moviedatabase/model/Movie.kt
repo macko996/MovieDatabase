30cf684bc715ca4559a494e2f4bbd19e1322548e
Marjan Kolevski <Marjan.Kolevski@netcetera.com>
2021-01-08 13:17:17 +0100

    Added models for TV Shows

--- app/src/main/java/com/example/moviedatabase/model/Movie.kt
+++ app/src/main/java/com/example/moviedatabase/model/Movie.kt
@@ -10,17 +10,11 @@ data class Movie(
     var title: String= "",
     @SerializedName("overview") var description: String = "",
     @SerializedName("poster_path") var posterPath: String = "",
-    var genres: ArrayList<MovieGenres> = ArrayList(),
+    var genres: ArrayList<Genres> = ArrayList(),
     @SerializedName("release_date") var releaseDate: String = "",
     @SerializedName("vote_average") var averageScore: Double = 0.0,
     var revenue : Int = 0,
     var runtime : Int = 0,
     var director: String = "",
     @SerializedName("backdrop_path") var backdropPath: String = ""
-)   {
-    data class MovieGenres(
-        var id: Int = 0,
-        var name: String = ""
-    )
-
-}
+)
